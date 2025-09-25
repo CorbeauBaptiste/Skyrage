78383275b123c2c004bb5cbc9b4693ee632554b3
CorbeauBaptiste <corbeaubaptiste2005@gmail.com>
2025-09-25 15:18:37 +0200

    fonction set get

--- unit/projectile.gd
+++ unit/projectile.gd
@@ -2,6 +2,7 @@ extends Area2D
 class_name Projectile
 
 @export var speed = 100: set = set_speed
+@export var damage = 1: set = set_damage
 
 func _ready():
 	set_as_top_level(true)
@@ -14,3 +15,6 @@ func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
 
 func set_speed(value):
 	speed = value
+
+func set_damage(value):
+	damage = value

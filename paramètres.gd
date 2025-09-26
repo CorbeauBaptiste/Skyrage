7b70bec099e5db2a045cf0f366a05f19ae6805eb
lucas210905 <lucas.debruyne2109@gmail.com>
2025-09-26 14:30:00 +0200

    interface v4 (fullscreen)

--- paramètres.gd
+++ paramètres.gd
@@ -1,5 +1,5 @@
 extends Control
 @onready var exit =preload("res://menuprincipal.tscn")
 
-func _on_button_exit_down() -> void:
+func _on_exit_button_down() -> void:
 	get_tree().change_scene_to_packed(exit)

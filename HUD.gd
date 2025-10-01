ce2b3aefc82439763badf26ecdcb75534cc84f54
corentindenis46 <deniscorentin46@gmail.com>
2025-10-01 17:26:33 +0200

    gold barre paradis fini

--- HUD.gd
+++ HUD.gd
@@ -1,45 +1,45 @@
 extends Control
 
-@onready var bar: ProgressBar              = %ElixirBar
-@onready var label: Label                  = %ElixirLabel
-@onready var btn2: Button                  = %BtnCost2
-@onready var btn4: Button                  = %BtnCost4
-@onready var btn6: Button                  = %BtnCost6
-@onready var elixir_manager: ElixirManager = %ElixirManager
+@onready var bar: ProgressBar              = %GoldBar
+@onready var label: Label                  = %GoldLabel
+@onready var btn2: Button                  = %BtnCost5
+@onready var btn4: Button                  = %BtnCost10
+@onready var btn6: Button                  = %BtnCost15
+@onready var gold_manager: goldManager = %GoldManager
 
-const COST_2 := 2.0
-const COST_4 := 4.0
-const COST_6 := 6.0
+const Cout_5 := 5.0
+const Cout_10 := 10.0
+const Cout_15 := 15.0
 
 func _ready() -> void:
 	bar.min_value = 0.0
-	bar.max_value = elixir_manager.max_elixir
-	_refresh_ui(elixir_manager.current_elixir, elixir_manager.max_elixir)
+	bar.max_value = gold_manager.max_gold
+	_refresh_ui(gold_manager.current_gold, gold_manager.max_gold)
 
-	elixir_manager.elixir_changed.connect(_on_elixir_changed)
-	elixir_manager.elixir_spent.connect(_on_elixir_spent)
-	elixir_manager.elixir_not_enough.connect(_on_elixir_not_enough)
+	gold_manager.gold_changed.connect(_on_gold_changed)
+	gold_manager.gold_spent.connect(_on_gold_spent)
+	gold_manager.gold_not_enough.connect(_on_gold_not_enough)
 
-	btn2.pressed.connect(func(): _try_spend(COST_2))
-	btn4.pressed.connect(func(): _try_spend(COST_4))
-	btn6.pressed.connect(func(): _try_spend(COST_6))
+	btn2.pressed.connect(func(): _try_spend(Cout_5))
+	btn4.pressed.connect(func(): _try_spend(Cout_10))
+	btn6.pressed.connect(func(): _try_spend(Cout_15))
 
 func _process(_delta: float) -> void:
-	var e = elixir_manager.current_elixir
-	btn2.disabled = e < COST_2
-	btn4.disabled = e < COST_4
-	btn6.disabled = e < COST_6
+	var e = gold_manager.current_gold
+	btn2.disabled = e < Cout_5
+	btn4.disabled = e < Cout_10
+	btn6.disabled = e < Cout_15
 
 func _try_spend(cost: float) -> void:
-	elixir_manager.spend(cost)
+	gold_manager.spend(cost)
 
-func _on_elixir_changed(current: float, max_value: float) -> void:
+func _on_gold_changed(current: float, max_value: float) -> void:
 	_refresh_ui(current, max_value)
 
-func _on_elixir_spent(_cost: float) -> void:
+func _on_gold_spent(_cost: float) -> void:
 	_pulse_bar()
 
-func _on_elixir_not_enough(cost: float) -> void:
+func _on_gold_not_enough(cost: float) -> void:
 	_shake_label("Pas assez d’élixir (coût: %d) !" % int(cost))
 
 func _refresh_ui(current: float, max_value: float) -> void:

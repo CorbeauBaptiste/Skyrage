cd6382f456a1f4d5f640e267543fcff5d2a6add8
Anir (yat) <contact@anirkm.dev>
2025-10-03 01:11:01 +0200

    ajout debug dans goldmanager

--- base/scritps/BaseEnfer.gd
+++ base/scritps/BaseEnfer.gd
@@ -1,14 +1,7 @@
-extends Base  # Hérite de Base.gd
+extends Base
 class_name BaseEnfer
 
-@export var texture_path: String = "res://textures/base_enfer.png"
-
 func _ready() -> void:
-	super._ready()  # Exécute Base
-	team = "enfer"  # Force team
-	$Sprite2D.texture = load(texture_path)
-	$Sprite2D.modulate = Color.RED  # Rouge infernal (GDD)
-	# Effet visuel : Ajoute GPUParticles2D enfant si tu veux (lave)
-	# player.nom = "Joueur Enfer"  # Déjà dans Base
-
-# Override si besoin (ex: sons démoniaques)
+	super._ready()
+	$Sprite2D.modulate = Color.RED
+	print("BaseEnfer visuels appliqués")
--- base/scritps/BaseParadis.gd
+++ base/scritps/BaseParadis.gd
@@ -1,1 +1,7 @@
-extends Node
+extends Base
+class_name BaseParadis
+
+func _ready() -> void:
+	super._ready()
+	$Sprite2D.modulate = Color.WHITE 
+	print("BaseParadis visuels appliqués")

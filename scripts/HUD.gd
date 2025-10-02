e21d4689000bd3bc9fd8f4dfb18e2c7d3eade214
corentindenis46 <deniscorentin46@gmail.com>
2025-10-02 22:02:12 +0200

    timer 30 secondes

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -5,11 +5,15 @@ extends Control
 @onready var btn2: Button                  = %BtnCost5
 @onready var btn4: Button                  = %BtnCost10
 @onready var btn6: Button                  = %BtnCost15
-@onready var gold_manager: goldManager = %GoldManager
+@onready var gold_manager: goldManager     = %GoldManager
 
-const Cout_5 := 5.0
+const Cout_5  := 5.0
 const Cout_10 := 10.0
 const Cout_15 := 15.0
+
+const PHASE_DURATION := 30.0
+var is_phase_on: bool = true
+var buttons_forced_disabled: bool = false
 
 func _ready() -> void:
 	bar.min_value = 0.0
@@ -18,29 +22,36 @@ func _ready() -> void:
 
 	gold_manager.gold_changed.connect(_on_gold_changed)
 	gold_manager.gold_spent.connect(_on_gold_spent)
-	gold_manager.gold_not_enough.connect(_on_gold_not_enough)
 
 	btn2.pressed.connect(func(): _try_spend(Cout_5))
 	btn4.pressed.connect(func(): _try_spend(Cout_10))
 	btn6.pressed.connect(func(): _try_spend(Cout_15))
 
+	_enter_phase(true)
+	_run_cycle()
+
 func _process(_delta: float) -> void:
-	var e = gold_manager.current_gold
-	btn2.disabled = e < Cout_5
-	btn4.disabled = e < Cout_10
-	btn6.disabled = e < Cout_15
+	if buttons_forced_disabled:
+		btn2.disabled = true
+		btn4.disabled = true
+		btn6.disabled = true
+	else:
+		var e = gold_manager.current_gold
+		btn2.disabled = e < Cout_5
+		btn4.disabled = e < Cout_10
+		btn6.disabled = e < Cout_15
 
 func _try_spend(cost: float) -> void:
+	if not is_phase_on:
+		return
 	gold_manager.spend(cost)
 
 func _on_gold_changed(current: float, max_value: float) -> void:
-	_refresh_ui(current, max_value)
+	if is_phase_on:
+		_refresh_ui(current, max_value)
 
 func _on_gold_spent(_cost: float) -> void:
 	_pulse_bar()
-
-func _on_gold_not_enough(cost: float) -> void:
-	_shake_label("Pas assez d’élixir (coût: %d) !" % int(cost))
 
 func _refresh_ui(current: float, max_value: float) -> void:
 	bar.value = current
@@ -49,7 +60,21 @@ func _refresh_ui(current: float, max_value: float) -> void:
 func _pulse_bar() -> void:
 	var tween := create_tween()
 
-
 func _shake_label(msg: String) -> void:
 	label.text = msg
 	var tween := create_tween()
+
+func _run_cycle() -> void:
+	while true:
+		await get_tree().create_timer(PHASE_DURATION).timeout
+		_enter_phase(not is_phase_on)
+
+func _enter_phase(phase_on: bool) -> void:
+	is_phase_on = phase_on
+	if is_phase_on:
+		buttons_forced_disabled = false
+		gold_manager.set_process(true)
+		_refresh_ui(gold_manager.current_gold, gold_manager.max_gold)
+	else:
+		buttons_forced_disabled = true
+		gold_manager.set_process(false)

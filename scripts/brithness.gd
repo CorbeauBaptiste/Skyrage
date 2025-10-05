d72ab361cdee696c80be1a1649f02d489fca1b66
Anir (yat) <contact@anirkm.dev>
2025-10-05 23:53:30 +0200

    fix: scene + brightness

--- scripts/brithness.gd
+++ scripts/brithness.gd
@@ -1,8 +1,9 @@
 extends HSlider
 
-func _ready() -> void:
-	value = Globals.brightness 
 
-func _on_value_changed(value: float) -> void:
-	GlobalWorldEnvironment.environment.adjustment_brightness = value
-	Globals.brightness = value
+func _ready() -> void:
+	value = Globals.brightness
+
+func _on_value_changed(new_value: float) -> void:
+	GlobalWorldEnvironment.environment.adjustment_brightness = new_value
+	Globals.brightness = new_value

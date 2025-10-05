c01238b9857b1e0869565ca441f2c59344352ef1
TarzanHR <leny621000@gmail.com>
2025-10-05 15:56:24 +0200

    add: folder from timer branch and timer scenes

---
+++ unit/projectile.gd
@@ -0,0 +1,47 @@
+extends Area2D
+class_name Projectile
+
+@export var speed = 100: set = set_speed
+@export var damage = 1: set = set_damage
+@export var targets_enfer = true: set = set_target
+
+func _ready():
+	set_as_top_level(true)
+	collision_mask = 2
+
+func _process(delta):
+	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
+
+func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
+	queue_free()
+
+func change_sprite(sprite_route, hframes = 0, vframes = 0, frame = 0):
+	var texture = load(sprite_route)
+	$Sprite2D.texture = texture
+	$Sprite2D.hframes = hframes
+	$Sprite2D.vframes = vframes
+	$Sprite2D.frame = frame
+
+func set_speed(value):
+	speed = value
+
+func set_damage(value):
+	damage = value
+
+func set_target(value):
+	targets_enfer = value
+
+func _on_body_entered(body: Node2D) -> void:
+	print("Projectile touché : ", body.get_class(), " (nom: ", body.name if body else "null", ")")
+	if body is Unit:
+		if body.has_method("get_side"): 
+			if targets_enfer and body.get_side() == true:
+				body.set_health(body.get_health() - damage)
+				queue_free()
+				print("Dmg infligé à unité")
+			elif not targets_enfer and not body.get_side():
+				body.set_health(body.get_health() - damage)
+				queue_free()
+				print("Dmg infligé à unité")
+	else:
+		print("Projectile ignore non-Unit : ", body.get_class())

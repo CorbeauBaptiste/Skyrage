dd8e3ecea8103eb4108da027afce8dc70060dc58
LOSAT Giovanni <giolstpro@gmail.com>
2025-10-24 16:08:33 +0200

    feat: ajout fonction pour la gestion des déplacements

--- scripts/entities/units/unit_paradis/seraphin/seraphin.gd
+++ scripts/entities/units/unit_paradis/seraphin/seraphin.gd
@@ -1,66 +1,101 @@
 extends Unit
 
-var _destination_position: Vector2  # Position de destination pour le déplacement
-@export var pos_base: Vector2 = Vector2(612.0, 727.0)
-@export var speed_sera: float = 20.0
-@export var att_cooldown: float = 0.50
-var can_attack: bool = true
-var current_enemy: Node = null  # Référence à l'ennemi actuel
-@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+@export var pos_base: Vector2 = Vector2(612.0, 727.0) # Position de la base approxi
+@export var speed_sera: float = 20.0 # Rapidité de l'unité
+@export var att_cooldown: float = 0.5 # Temps de rechargement entre chaque attaques 
+@export var avoid_time: float = 1.0 # durée d'esquives
+@export var avoid_distance: float = 100.0 # distance de l'esquive
+
+var can_attack := true # entrain d'attaquer
+var avoiding := false  # entrain d'esquiver
+var avoid_timer := 0.0
+
 
 func _ready():
 	set_health(1600)
-	_destination_position = pos_base
-	if navigation_agent:
-		navigation_agent.target_position = _destination_position
-	else:
-		push_error("NavigationAgent2D non trouvé !")
+	set_speed(speed_sera)
+	target = pos_base
 	$Timer.timeout.connect(_if_attcooldown_end)
 
-func _physics_process(delta: float):
+func _physics_process(delta):
+	if avoiding:
+		avoid_timer -= delta
+		if avoid_timer <= 0.0:
+			avoiding = false
+			target = pos_base
+	else:
+		_move_to()
+	var last_pos = global_position
+	
 	super._physics_process(delta)
-	if navigation_agent:
-		if navigation_agent.is_target_reached():
-			navigation_agent.target_position = _destination_position
-		if !navigation_agent.is_target_reached() and !navigation_agent.is_navigation_finished():
-			var next_path_position = navigation_agent.get_next_path_position()
-			var direction = (next_path_position - global_position).normalized()
-			velocity = direction * speed_sera
-			move_and_slide()
+
+	if global_position.distance_to(last_pos) < 0.1 and not avoiding:
+		_start_avoidance()
+	
+	if global_position.distance_to(pos_base) < 10.0:
+		target = null
+		velocity = Vector2.ZERO
+
 	if can_attack:
 		_check_and_attack()
 
+# fonction pour calculer une cible intermédiaire
+func _move_to():
+	if target == null:
+		target = pos_base
+		
+	var direction = (pos_base - global_position).normalized()
+	target = global_position + direction * 60.0
+
+# fonction qui calcule la distance face à un obstacle
+func _start_avoidance():
+	avoiding = true
+	avoid_timer = avoid_time
+
+	var to_base = (pos_base - global_position).normalized()
+
+	# Directions latérales gauche/droite 
+	var left_dir = Vector2(-to_base.y, to_base.x)
+	var right_dir = Vector2(to_base.y, -to_base.x)
+
+	# On évalue laquelle rapproche le plus de la base
+	var left_pos = global_position + left_dir * avoid_distance
+	var right_pos = global_position + right_dir * avoid_distance
+
+	var left_dist = left_pos.distance_to(pos_base)
+	var right_dist = right_pos.distance_to(pos_base)
+
+	# Comparaison entre les deux distances
+	var best_dir = right_dir if right_dist < left_dist else left_dir
+	target = global_position + best_dir * avoid_distance
+
+
+# fonction pour vérifie la présence d'un ennemis autour de l'unité
 func _check_and_attack():
 	var enemies = $Range.get_overlapping_bodies()
 	for body in enemies:
 		if is_instance_valid(body) and body != self and body.has_method("get_side") and body.get_side() != self.get_side():
-			current_enemy = body
-			attack(current_enemy)
+			attack(body)
 			can_attack = false
 			$Timer.start(att_cooldown)
 			break
 
+# fonction qui permets à l'unité d'attaquer lorsque le cooldown est fini
 func _if_attcooldown_end():
 	can_attack = true
 
-func attack(enemy: Node):
-	if not is_instance_valid(enemy):
-		return
-	var enemy_pos = enemy.global_position
-	$Marker2D.look_at(enemy_pos)
+# permets de gérer les attaque sur les ennemies
+func attack(target_body):
+	var target_pos = target_body.global_position
+	$Marker2D.look_at(target_pos)
+
 	var arrow_instance = arrow.instantiate()
 	arrow_instance.change_sprite("res://assets/sprites/projectiles/vent.png")
 	arrow_instance.is_cupidon_arrow = true
 	arrow_instance.area_damage = 600
 	arrow_instance.area_radius = 20
 	arrow_instance.set_target(not self.get_side())
+
 	arrow_instance.rotation = $Marker2D.rotation
 	arrow_instance.global_position = $Marker2D.global_position
 	add_child(arrow_instance)
-	print("Attaque sur : ", enemy.name)
-
-# Fonction pour définir la destination
-func set_destination(new_destination: Vector2):
-	_destination_position = new_destination
-	if navigation_agent:
-		navigation_agent.target_position = _destination_position
